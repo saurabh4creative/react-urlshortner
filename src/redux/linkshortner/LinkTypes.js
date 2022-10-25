eae720e8dd3e8dd6376775d7b64cc55742f47e8b
saurabhjain9070 <tiwefe7341@0pppp.com>
2022-10-25 22:34:13 +0530

    Add API End Points

--- src/redux/linkshortner/LinkTypes.js
+++ src/redux/linkshortner/LinkTypes.js
@@ -1,5 +1,5 @@
-export const API_URL   = 'http://localhost:3001';
-export const BASE_URL  = 'http://localhost:3001';
+export const API_URL   = 'https://react-url-shortner.glitch.me';
+export const BASE_URL  = 'https://react-urlshortners.netlify.app';
 export const ADD_LINK  = 'ADD_LINK';
 export const URL_LINK  = 'URL_LINK';
 export const ERROR_URL = 'ERROR_URL';
